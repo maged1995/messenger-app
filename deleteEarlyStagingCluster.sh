4db8ba0cb4984e030746e28beaaeafbfbfed6ad5
Maged A. Saad <magedsaadaziz@hotmail.com>
2022-02-26 22:31:35 +0200

    undo temporary funtion

--- deleteEarlyStagingCluster.sh
+++ deleteEarlyStagingCluster.sh
@@ -3,17 +3,17 @@
 STAGING_CLUSTER_NAME=$(aws eks list-clusters | jq -r '.clusters[0]')
 OWNER_ID=371713243830
 
-# ROLE_ARN=$(eksctl get iamidentitymapping --cluster $STAGING_CLUSTER_NAME --region us-east-1 -o json | jq -r '.[0].rolearn')
-#
-# aws iam detach-role-policy \
-#   --role-name ${ROLE_ARN:31} \
-#   --policy-arn arn:aws:iam::$OWNER_ID:policy/AWSLoadBalancerControllerIAMPolicy
-#
-# LB_ARN=$(aws elbv2 describe-load-balancers --query 'sort_by(LoadBalancers, &CreatedTime)' | jq -r '.[0].LoadBalancerArn')
-#
-# # aws elbv2 delete-load-balancer --load-balancer-arn $LB_ARN
-#
-# eksctl delete nodegroup --cluster $STAGING_CLUSTER_NAME --name linux-nodes --region us-east-1
+ROLE_ARN=$(eksctl get iamidentitymapping --cluster $STAGING_CLUSTER_NAME --region us-east-1 -o json | jq -r '.[0].rolearn')
+
+aws iam detach-role-policy \
+  --role-name ${ROLE_ARN:31} \
+  --policy-arn arn:aws:iam::$OWNER_ID:policy/AWSLoadBalancerControllerIAMPolicy
+
+LB_ARN=$(aws elbv2 describe-load-balancers --query 'sort_by(LoadBalancers, &CreatedTime)' | jq -r '.[0].LoadBalancerArn')
+
+# aws elbv2 delete-load-balancer --load-balancer-arn $LB_ARN
+
+eksctl delete nodegroup --cluster $STAGING_CLUSTER_NAME --name linux-nodes --region us-east-1
 
 # aws cloudformation delete-stack --stack-name "eksctl-${STAGING_CLUSTER_NAME}-nodegroup-linux-nodes"
 
